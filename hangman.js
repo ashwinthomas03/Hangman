9ebdd4a656c1491d8cb749471bac909cffed7b4c
ashwinthomas03 <118752427+ashwinthomas03@users.noreply.github.com>
2025-10-20 10:05:00 -0400

    first version of new game logic

--- hangman.js
+++ hangman.js
@@ -1,4 +1,20 @@
 var POSSIBLE_WORDS = ["obdurate", "verisimilitude", "defenestrate", "obsequious", "dissonant", "toady", "idempotent"];
 
-var randomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length);
-var word = POSSIBLE_WORDS[randomIndex];
+var word = "";
+
+// start new games 
+// Write out the spaces for the letters using for loop
+// display the string of _ in html
+
+function newGame(){
+    var randomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length);
+    word = POSSIBLE_WORDS[randomIndex];
+
+    var clueString = "";
+    for (var i = 0; i < word.length; i++){
+        clueString += "_ ";
+    }
+
+    var clue = document.getElementById("clue");
+    clue.innerHTML = clueString;
+}
